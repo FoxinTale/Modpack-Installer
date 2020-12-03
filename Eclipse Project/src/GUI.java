4a5a0f3321fe62942d2a12e52ed3dbf4f2222acf
Aubrey <51178662+FoxinTale@users.noreply.github.com>
2020-12-02 20:14:26 -0500

    Added background colours.

--- Eclipse Project/src/GUI.java
+++ Eclipse Project/src/GUI.java
@@ -29,18 +29,22 @@ public class GUI {
         JLabel errorsLabel = new JLabel(Strings.installerErrorsLabelText);
         ButtonGroup options = new ButtonGroup();
 
+        JPanel lightRedPanel = new JPanel();
+        JPanel lightOrangePanel = new JPanel();
+        JPanel lightYellowPanel = new JPanel();
+        JPanel lightGreenPanel = new JPanel();
+        JPanel lightBluePanel = new JPanel();
+        JPanel lightPurplePanel = new JPanel();
+        JPanel lightVioletPanel = new JPanel();
+
         JLabel installerVersion = new JLabel(Strings.installerVersionText+ installerVersionValue);
         consoleOutput.setLineWrap(true);
-
         frame.getContentPane().add(scroll);
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
         progress.setMaximum(100000);
-
         scroll.setBorder(new LineBorder(Color.black, 1, true));
 
         Color rbc = new Color(220, 255, 255); // Hex value: dcffff
-        Color lightRed = new Color(255, 129, 127);
 
         options.add(modpackOne);
         options.add(downloadOption);
@@ -132,38 +136,52 @@ public class GUI {
                 Errors.makeGUI();
             }
         };
+        Color lightRed = new Color(255, 188, 188);
+        Color lightOrange = new Color(255, 216, 158, 255);
+        Color lightYellow = new Color(255, 255, 200);
+        Color lightGreen = new Color(203, 234, 203);
+        Color lightBlue = new Color(198, 215, 222);
+        Color lightPurple = new Color(220, 189, 218);
+        Color lightViolet = new Color(192, 148, 189);
 
-
-        errorLookup.setBackground(lightRed);
         frame.setTitle(Strings.installerWindowTitle);
 
-        errorLookup.setBounds(415, 300, 50, 50); // x axis, y axis, width, height
-        scroll.setBounds(30, 20, 400, 200);
-
-        modpackOne.setBounds(120, 235, 200, 15);
-        modpackPanel.setBounds(115, 230, 250, 25);
-
-        downloadOption.setBounds(120, 270, 200, 15);
-        downloadPanel.setBounds(115, 265, 250, 25);
-
-        updatePanel.setBounds(115, 300, 250, 25);
-
-        resourceOption.setBounds(120, 305, 200, 15);
-        resourcePanel.setBounds(115, 335, 250, 25);
-        otherOptions.setBounds(120, 340, 200, 15);
-
-        progress.setBounds(25, 475, 275, 25); // X, Y, Width, Height
-        installerVersion.setBounds(195, 510, 100, 20); // 190
-        button.setBounds(325, 475, 100, 25);
-
-        errors.setBounds(75, 440, 350, 20);
-        errorsLabel.setBounds(25, 440, 50, 20);
-
-        // Radio Button Colour
+        //Setting background colours.
         modpackOne.setBackground(rbc);
         downloadOption.setBackground(rbc);
         otherOptions.setBackground(rbc);
         resourceOption.setBackground(rbc);
+        lightRedPanel.setBackground(lightRed);
+        lightOrangePanel.setBackground(lightOrange);
+        lightYellowPanel.setBackground(lightYellow);
+        lightGreenPanel.setBackground(lightGreen);
+        lightBluePanel.setBackground(lightBlue);
+        lightPurplePanel.setBackground(lightPurple);
+        lightVioletPanel.setBackground(lightViolet);
+
+        //Setting the location of each element.
+        lightRedPanel.setBounds(0,0,70,575);
+        lightOrangePanel.setBounds(70,0,70,575);
+        lightYellowPanel.setBounds(140,0,70,575);
+        lightGreenPanel.setBounds(210, 0, 70, 575);
+        lightBluePanel.setBounds(280, 0, 70, 575);
+        lightPurplePanel.setBounds(350, 0, 70, 575);
+        lightVioletPanel.setBounds(420, 0, 70,575);
+        errorLookup.setBounds(415, 300, 50, 50); // x axis, y axis, width, height
+        scroll.setBounds(30, 20, 400, 200);
+        modpackOne.setBounds(120, 235, 200, 15);
+        modpackPanel.setBounds(115, 230, 250, 25);
+        downloadOption.setBounds(120, 270, 200, 15);
+        downloadPanel.setBounds(115, 265, 250, 25);
+        updatePanel.setBounds(115, 300, 250, 25);
+        resourceOption.setBounds(120, 305, 200, 15);
+        resourcePanel.setBounds(115, 335, 250, 25);
+        otherOptions.setBounds(120, 340, 200, 15);
+        progress.setBounds(25, 475, 275, 25); // X, Y, Width, Height
+        installerVersion.setBounds(195, 510, 100, 20); // 190
+        button.setBounds(325, 475, 100, 25);
+        errors.setBounds(75, 440, 350, 20);
+        errorsLabel.setBounds(25, 440, 50, 20);
 
         modpackPanel.add(modpackOne);
         downloadPanel.add(downloadOption);
@@ -185,6 +203,7 @@ public class GUI {
         button.addActionListener(buttonEvent);
         errorLookup.addActionListener(errorsEvent);
 
+        // Adding all the elements to the frame.
         frame.add(button);
         frame.add(scroll);
         frame.add(progress);
@@ -201,10 +220,15 @@ public class GUI {
         frame.add(resourceOption);
         frame.add(resourcePanel);
         frame.add(errorLookup);
+        frame.add(lightRedPanel);
+        frame.add(lightOrangePanel);
+        frame.add(lightYellowPanel);
+        frame.add(lightGreenPanel);
+        frame.add(lightBluePanel);
+        frame.add(lightPurplePanel);
+        frame.add(lightVioletPanel);
 
         errorLookup.setVisible(false);
-
-
         frame.setSize(480, 575);
         frame.setResizable(false);
 
